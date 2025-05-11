c212aa99e785d0404f414111990a1d3c3b7116cf
gctoledo <gabrielcostatoledo@outlook.com>
2025-05-10 22:48:43 -0300

    feat: add error response dtos

--- src/main/java/dev/gabrieltoledo/kito/web/dtos/response/ErrorResponseDTO.java
+++ src/main/java/dev/gabrieltoledo/kito/web/dtos/response/ErrorResponseDTO.java
@@ -1,10 +1,21 @@
 package dev.gabrieltoledo.kito.web.dtos.response;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 
-public record ErrorResponseDTO(int status, String message) {
-    public static ErrorResponseDTO conflictResponse(String message) {
-        return new ErrorResponseDTO(HttpStatus.CONFLICT.value(), message);
+public record ErrorResponseDTO(int status, String message, List<FieldErrorsDTO> errors) {
+
+    public static ErrorResponseDTO badRequestResponse(String message, List<FieldErrorsDTO> errors) {
+        return new ErrorResponseDTO(HttpStatus.BAD_REQUEST.value(), message, errors);
+    }
+
+    public static ErrorResponseDTO conflictResponse(String message, List<FieldErrorsDTO> errors) {
+        return new ErrorResponseDTO(HttpStatus.CONFLICT.value(), message, errors);
+    }
+
+    public static ErrorResponseDTO notFoundResponse(String message, List<FieldErrorsDTO> errors) {
+        return new ErrorResponseDTO(HttpStatus.NOT_FOUND.value(), message, errors);
     }
 
 }
---
+++ src/main/java/dev/gabrieltoledo/kito/web/dtos/response/FieldErrorsDTO.java
@@ -0,0 +1,4 @@
+package dev.gabrieltoledo.kito.web.dtos.response;
+
+public record FieldErrorsDTO(String field, String message) {
+}
