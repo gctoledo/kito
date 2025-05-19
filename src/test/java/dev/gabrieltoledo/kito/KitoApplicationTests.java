2656f41347615a420b31e94be8826e1621852daf
gctoledo <gabrielcostatoledo@outlook.com>
2025-05-19 12:17:02 -0300

    chore: add initial application properties

--- src/test/java/dev/gabrieltoledo/kito/KitoApplicationTests.java
+++ src/test/java/dev/gabrieltoledo/kito/KitoApplicationTests.java
@@ -3,7 +3,7 @@ package dev.gabrieltoledo.kito;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.profiles.active=test"})
 class KitoApplicationTests {
 
 	@Test
