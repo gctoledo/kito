7d76f59bbd42a0e10cbf16b8895ad351a0a621d3
gctoledo <gabrielcostatoledo@outlook.com>
2025-05-21 12:51:05 -0300

    chore: enable jpa auditing

--- src/main/java/dev/gabrieltoledo/kito/KitoApplication.java
+++ src/main/java/dev/gabrieltoledo/kito/KitoApplication.java
@@ -2,10 +2,8 @@ package dev.gabrieltoledo.kito;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class KitoApplication {
 
 	public static void main(String[] args) {
